566e0df45da18eb60f27535d6df1f7c5b15a0d26
timaaagoo1 <157299229+timaaagoo1@users.noreply.github.com>
2025-06-20 20:05:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,29 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))  # Render использует переменную PORT
     app.run(host='0.0.0.0', port=port)
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# ⚙️ логин и пароль (их будет генерировать бот позже)
+correct_username = "user123"
+correct_password = "pass456"
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == correct_username and password == correct_password:
+            return redirect(url_for('home'))
+        else:
+            error = 'Неверный логин или пароль!'
+    return render_template('login.html', error=error)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
