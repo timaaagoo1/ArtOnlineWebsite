aa0825d166eb51acfff1426a6763b9ef6e73e33c
timaaagoo1 <157299229+timaaagoo1@users.noreply.github.com>
2025-06-20 19:57:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hello from Render!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Привет! Это мой сайт, и он работает!'
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render использует переменную PORT
+    app.run(host='0.0.0.0', port=port)
