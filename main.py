84bdd7fe2a198770dbbeca3389b5a80c94148cd8
timaaagoo1 <157299229+timaaagoo1@users.noreply.github.com>
2025-06-20 20:09:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,5 +22,7 @@ def login():
 def home():
     return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run()
+ if __name__ == '__main__':
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
+
